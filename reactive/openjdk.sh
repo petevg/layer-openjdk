29e093840a61bfa5daf921a2608437be74c7a15f
Kevin W Monroe <kevin.monroe@canonical.com>
2016-03-10 07:24:36 +0000

    combine common install/config-changed pieces into one func; use local-scoped vars; handle java-major config-changed as a re-install

--- reactive/openjdk.sh
+++ reactive/openjdk.sh
@@ -1,85 +1,89 @@
 #!/bin/bash
+source charms.reactive.sh
 set -ex
 
-source charms.reactive.sh
+# Update java-related system configuration and relation data. This function
+# must only be called from a 'java.connected' state handler.
+#
+# :param: The major java version (e.g.: 6)
+function update_java_data() {
+    local java_major=$1
+    local java_alternative=$(update-java-alternatives -l | grep java-1.${java_major} | awk {'print $1'})
 
-# Remove any previous mention of JAVA_HOME, then append the appropriate value
-# based on the source of our /usr/bin/java symlink (if it exists).
-function update_java_home() {
+    # Set our java symlinks to the alternative that matched our major version.
+    update-java-alternatives -s ${java_alternative}
+
+    # Remove any previous mention of JAVA_HOME from /etc/environment.
     sed -i -e '/JAVA_HOME/d' /etc/environment
 
+    # Update environment and relation if we have a /usr/bin/java symlink
     if [[ -L "/usr/bin/java" ]]; then
-        java_home=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
+        local java_home=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
+        local java_version=$(java -version 2>&1 | grep -i version | head -1 | awk -F '"' {'print $2'})
         echo "JAVA_HOME=${java_home}" >> /etc/environment
+        relation_call --state=java.connected set_ready $java_home $java_version
     fi
 }
 
 @when 'java.connected'
 @when_not 'java.installed'
 function install() {
-    install_type=$(config-get 'install-type')
-    java_major=$(config-get 'java-major')
+    local install_type=$(config-get 'install-type')
+    local java_major=$(config-get 'java-major')
 
-    # Install jre or jdk+jre depending on config. Since this is the first time
-    # we're installing (when_not java.installed), update-alternatives will be
-    # handled by the deb.
-    apt-get update -q
+    # Install jre or jdk+jre depending on config.
     status-set maintenance "Installing OpenJDK ${java_major} (${install_type})"
+    apt-get update -q
     if [[ ${install_type} == "full" ]]; then
       apt-get install -qqy openjdk-${java_major}-jre-headless openjdk-${java_major}-jdk
     else
       apt-get install -qqy openjdk-${java_major}-jre-headless
     fi
-    update_java_home
 
-    # Send relation data
-    java_home=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
-    java_version=$(java -version 2>&1 | grep -i version | head -1 | awk -F '"' {'print $2'})
-    relation_call --state=java.connected set_ready $java_home $java_version
-
+    # Register current java information
+    update_java_data $java_major
     set_state 'java.installed'
     status-set active "OpenJDK ${java_major} (${install_type}) installed"
 }
 
 @when 'java.connected' 'java.installed'
-function check_version() {
-    install_type=$(config-get 'install-type')
-    java_major=$(config-get 'java-major')
-    java_major_installed=$(java -version 2>&1 | grep -i version | head -1 | awk -F '.' {'print $2'})
+@when 'config.changed.java-major'
+function change_major() {
+    # Different major java version requested by config, call install.
+    # NOTE: no need to check for an install-type change when java-major changes.
+    # The install function will use the current config value whether it
+    # changed since initial install or not.
+    install
+}
 
-    # Install new major version if the user has set 'java-major' to something
-    # different than the version we have installed.
-    if [[ $java_major != $java_major_installed ]]; then
-        status-set maintenance "Installing OpenJDK ${java_major} (${install_type})"
-        apt-get update -q
-        if [[ ${install_type} == "full" ]]; then
-          apt-get install -qqy openjdk-${java_major}-jre-headless openjdk-${java_major}-jdk
-        else
-          apt-get install -qqy openjdk-${java_major}-jre-headless
-        fi
-        status-set active "OpenJDK ${java_major} (${install_type}) installed"
-        relation_call --state=java.connected set_ready $java_home $java_version
-    elif [[ ${install_type} == 'jre' ]]; then
-      # Remove the JDK if it exists but config tells us we only want the JRE
+@when 'java.connected' 'java.installed'
+@when 'config.changed.install-type'
+@when_not 'config.changed.java-major'
+function change_type() {
+    # Different install-type (but same java-major) requested by config.
+    # Update packages accordingly.
+    # NOTE: if install-type AND java-major change, that is handled with a
+    # reinstall in the above change_major function.
+    local install_type=$(config-get 'install-type')
+    local java_major=$(config-get 'java-major')
+
+    if [[ ${install_type} == 'jre' ]]; then
+      # Config tells us we only want the JRE. Remove the JDK if it exists.
       if dpkg -s openjdk-${java_major}-jdk &> /dev/null; then
         status-set maintenance "Uninstalling OpenJDK ${java_major} (JDK)"
         apt-get remove --purge -qqy openjdk-${java_major}-jdk
       fi
     elif [[ ${install_type} == 'full' ]]; then
-      # Install the JDK if config tells us we want a full install (it doesn't
-      # hurt to install a package that is already installed. NOTE: this will
-      # update any existing jdk package to the latest point release).
+      # Config tells us we want a full install. Install the JDK unconditionally
+      # (it doesn't hurt to install a package that is already installed).
+      # NOTE: this will update existing jdk packages to the latest rev of the
+      # major release.
       status-set maintenance "Installing OpenJDK ${java_major} (${install_type})"
       apt-get install -qqy openjdk-${java_major}-jdk
     fi
 
-    # Unconditionally switch all java-related symlinks to our major version.
-    # This doesn't hurt anything even if we didn't make any package changes.
-    # It helps ensure our system symlinks are always right, especially if we
-    # changed major version or install type.
-    java_alternative=$(update-java-alternatives -l | grep java-1.${java_major} | awk {'print $1'})
-    update-java-alternatives -s ${java_alternative}
-    update_java_home
+    # Register current java information
+    update_java_data $java_major
     status-set active "OpenJDK ${java_major} (${install_type}) installed"
 }
 
@@ -89,7 +93,6 @@ function uninstall() {
     # Uninstall all versions of OpenJDK
     status-set maintenance "Uninstalling OpenJDK (all versions)"
     apt-get remove --purge -qqy openjdk-[0-9]?-j.*
-    update_java_home
 
     remove_state 'java.installed'
     status-set blocked "OpenJDK (all versions) uninstalled"
